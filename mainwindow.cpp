747fc547e2baa0148559d17065364c07fe4c666a
WinNice98 <105580070+WinNice98@users.noreply.github.com>
2025-02-11 23:58:44 +0300

    created project
    add some interface on main game

---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QMovie *movie = new QMovie(":/gif/defback.gif");
+    ui->background->setMovie(movie);
+    ui->background->setScaledContents(true);
+    QString dial = "Test Dialog! \nGood Luck, Have Fun!\n";
+    QString stat = "Test Status! \nGood Luck, Have Fun!\n";
+    ui->text->setText(dial);
+    ui->stats->setText(stat);
+    movie->start();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_background_linkActivated(const QString &link)
+{
+
+}
+
